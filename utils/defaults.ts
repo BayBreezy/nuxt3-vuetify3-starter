e55aacb3df49293b9a4205398a1367771dd543f0
Behon Baker <behon.baker@yahoo.com>
2024-05-05 22:12:59 -0500

    chore(types): update import path for `DefaultsInstance`

--- utils/defaults.ts
+++ utils/defaults.ts
@@ -1,21 +1,21 @@
-import { DefaultsInstance } from "vuetify/lib/framework.mjs";
+import type { DefaultsInstance } from "vuetify";
 
 /**
  * A few defaults that I like
  */
 export const defaults: DefaultsInstance = {
-	VAppBar: {
-		elevation: 0,
-	},
-	VBtn: {
-		variant: "flat",
-		height: 38,
-		rounded: "lg",
-		size: "small",
-	},
-	VTextField: {
-		color: "primary",
-		variant: "outlined",
-		density: "comfortable",
-	},
+  VAppBar: {
+    elevation: 0,
+  },
+  VBtn: {
+    variant: "flat",
+    height: 38,
+    rounded: "lg",
+    size: "small",
+  },
+  VTextField: {
+    color: "primary",
+    variant: "outlined",
+    density: "comfortable",
+  },
 };
